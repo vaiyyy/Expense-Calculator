86bc41dd985526f8780ffd82adc01a3ea06fd2e6
vaiyyy <vaib.just@gmail.com>
2021-03-31 05:34:10 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+import Budget from './components/Budget';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ExpenseItem from './components/list/ExpenseItem';
+import ExpenseList from './components/list/ExpenseList';
+import AddExpenseForm from './components/forms/AddExpense';
+import {AppProvider} from './context/AppContext';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <AppProvider>
+    <div className="container">
+      <h1 className="mt-3">Budget Planner</h1>
+      <div className="row mt-3">
+        <div className="col-sm">
+         <Budget/>
+        </div>
+        <div className="col-sm">
+          <Remaining/>
+        </div>
+        <div className="col-sm">
+          <ExpenseTotal/>
+        </div>
+      </div>
+      <h3 className="mt-3">Expenses</h3>
+      <div className="row mt-3">
+        <div className="col-sm">
+          <ExpenseList/>
+        </div>
+      </div>
+      <h3 className='mt-3'>Add Expense</h3>
+      <div className='row mt-3'>
+        <div className='col-sm'>
+          <AddExpenseForm/>
+        </div>
+      </div>
     </div>
+    </AppProvider>
   );
-}
-
+};
 export default App;
